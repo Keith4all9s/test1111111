5b9a689d2a76c20fd80f2bec5f301788384fae3f
Keith4all9s <331245881@qq.com>
2018-01-22 16:38:45 +0800

    1add

---
+++ 自定义索引/自定义索引/1111v.m
@@ -0,0 +1,21 @@
+//
+//  1111v.m
+//  自定义索引
+//
+//  Created by LJ_Keith on 2018/1/22.
+//  Copyright © 2018年 Keith. All rights reserved.
+//
+
+#import "1111v.h"
+
+@implementation _111v
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
